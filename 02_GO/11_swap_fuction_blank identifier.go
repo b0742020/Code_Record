0b3d15047ad85c0970c98e91e31d049e039ba5d6
Ming <school83025@gmail.com>
2024-10-16 18:58:43 +0800

    12_Array&Slice.go Modified

--- 02_GO/11_swap_fuction_blank identifier.go
+++ 02_GO/11_swap_fuction_blank identifier.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func foo() (int, int) {
 	return 10, 20
@@ -32,4 +34,44 @@ func main() {
 
 	var username, _ = getUserinfo()
 	fmt.Println(username)
+
+	num := 10
+	fmt.Printf("num = %b\n", num)
+
+	m1 := 8.2
+	m2 := 3.5
+	fmt.Println(m1 + m2)
+
+	// var str = "123-456-789"
+	// arr := strings.Split(str, "-")
+	// str2 := strings.Join(arr, "*")
+	// fmt.Println(str, arr, str2)
+
+	// arr := []string{"php", "golang", "python"}
+	// str3 := strings.Join(arr, "-")
+	// fmt.Printf("%T\n", str3)
+	// fmt.Println(str3)
+
+	// str1 := "hello world"
+	// str2 := "w"
+	// num = strings.LastIndex(str1, str2)
+	// fmt.Println(num)
+
+	s := "hello world"
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%c ", s[i])
+	}
+	fmt.Println()
+
+	//修改英文字串 用byte(ascii)
+	s1 := "hello world"
+	byteStr := []byte(s1)
+	byteStr[4] = ' '
+	fmt.Println(string(byteStr))
+
+	//修改中文字串 用rune(utf8)
+	s2 := "台灣"
+	runeStr := []rune(s2)
+	runeStr[0] = '臺'
+	fmt.Println(string(runeStr))
 }
