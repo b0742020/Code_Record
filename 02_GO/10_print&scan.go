59301f964c93eca1940b0332f084b3814dc1da56
Ming <school83025@gmail.com>
2024-10-29 21:47:03 +0800

    Modify

--- 02_GO/10_print&scan.go
+++ 02_GO/10_print&scan.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func sayHello(x string) {
@@ -17,19 +18,35 @@ func sum() {
 }
 
 func main() {
-	// var x string = ""
-	// var stockcode = 940
-	// var enddate = "2021-12-31"
-	// var url = "Code = %d & endDate= %s"
-	// fmt.Printf(url, stockcode, enddate)
-	// fmt.Print("\n")
-	// fmt.Print("Enter your name: ")
-	// fmt.Scan(&x)
-	// fmt.Println(len(x))
-	// sayHello(x)
-	// sum()
-	// var a float32 = 3.222222
-	// var b int32 = 4
-	// fmt.Println(a + float32(b))
+	/*FormatInt
+	first parameter is  int64 format
+	second  parameter is what your base like binary octal decimal hexdecimal etc....
+	*/
+	// var i int = 10
+	// str1 := strconv.FormatInt(int64(i), 10)
+	// fmt.Println(str1)
+	//////////////////////////////////////////////////////
+	/*FormatFloat
+	1st  要轉換的值
+	2nd  轉換後的格式
+	3rd  要轉換的精度
+	4th  轉換的位數
+	*/
+	// var f float32 = 20.2333333
+	// str2 := strconv.FormatFloat(float64(f), 'f', 6, 32)
+	// fmt.Printf("Value : %v Type : %T\n", str2, str2)
+	//////////////////////////////////////////////////////
+	/*String convert to int
+	 */
+	// 	str := "123456"
+	// 	fmt.Printf("Value : %v Type : %T\n", str, str)
+	// 	num, _ := strconv.ParseInt(str, 10, 64)
+	// 	fmt.Printf("Value : %v Type : %T\n", num, num)
+	/*String convert to float
+	 */
+	//////////////////////////////////////////////////////
+	str := "123456.33333333"
+	num, err := strconv.ParseFloat(str, 64)
+	fmt.Printf("Value : %v Type : %T Value : %v\n", num, num, err)
 
 }
