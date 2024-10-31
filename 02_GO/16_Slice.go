21cf16dafb5a8573204fc8d72f9e571d5906bb9f
Ming <school83025@gmail.com>
2024-10-31 22:05:38 +0800

    Modify

---
+++ 02_GO/16_Slice.go
@@ -0,0 +1,36 @@
+package main
+
+func main() {
+	// 	var arr = [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// 	for k, v := range arr {
+	// 		fmt.Printf("arr[%d] = %d\n", k, v)
+	// 	}
+	// 	var arr1 = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// 	for k, v := range arr1 {
+	// 		fmt.Printf("arr1[%d] = %d\n", k, v)
+	// 	}
+	// 	var arr2 []int
+	// 	var arr3 = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// 	fmt.Println(arr2, arr3)
+	// 	fmt.Println(arr2 == nil, arr3 == nil)
+	// a := [5]int{55, 56, 57, 58, 59}
+	// fmt.Println(a)
+	// b := a[1:4]
+	// fmt.Println(b)
+	// c := a[2:]
+	// fmt.Println(c)
+	// d := a[:3]
+	// fmt.Println(d)
+	/*Slice 長度與容量*/
+	//長度: Slice的所包含的元素個數
+	//容量: Slice的所包含的第一個元素到底層陣列元素的個數
+	// s := []int{2, 3, 88, 4, 9, 77, 86, 98}
+	// fmt.Printf("長度 : %v 容量 : %v\n", len(s), cap(s))
+	// a := s[2:]
+	// fmt.Printf("長度 : %v 容量 : %v\n", len(a), cap(a))
+	// b := s[1:3]
+	// fmt.Printf("長度 : %v 容量 : %v\n", len(b), cap(b))
+	// c := s[:3]
+	// fmt.Printf("長度 : %v 容量 : %v\n", len(c), cap(c))
+
+}
